153b40135a8b4ae7f919dc245db79a45a120183b
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-07-27 18:21:43 -0300

    [BOTF-4] Style Schema (#2)
    ## [BOTF-4] Description
    Created the base for the style schema.

---
+++ src/style/darkTheme.js
@@ -0,0 +1,43 @@
+'use client';
+
+import { createTheme } from '@mui/material/styles';
+
+export const darkTheme = createTheme({
+   palette: {
+      mode: 'dark',
+      primary: {
+         main: '#2EB7E5',
+         light: '#5CC7EB'
+      },
+      secondary: {
+         main: '#0C3845'
+      },
+      tertiary: {
+         main: '#30D974',
+         light: '#6CE49D'
+      },
+      background: {
+         default: '#212A2D',
+         paper: '#252A2A'
+      },
+      text: {
+         primary: '#fafafa',
+         secondary: '#0F282F',
+         tertiary:'#254331',
+         warn: '#3A290E',
+         disabled: 'rgba(250,250,250,0.7)'
+      },
+      error: {
+         main: '#EF664B'
+      },
+      info: {
+         main: 'rgba(250,250,250,0.8)'
+      },
+      success: {
+         main: '#5CE091'
+      },
+      warn: {
+         main: '#D7C150'
+      }
+   }
+});
