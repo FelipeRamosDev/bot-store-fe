56aabf9d27e8d7264567d8d7ea7fbebbb3e8b48c
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-07-27 23:57:54 -0300

    [BOTF-7] Top Navigation Menu (#5)
    ## [BOTF-7] Description
    Created the to menu navigation

--- src/components/footers/mainFooter/MainFooter.jsx
+++ src/components/footers/mainFooter/MainFooter.jsx
@@ -1,7 +1,5 @@
-import './MainFooter.scss';
-
 export default function MainFooter() {
    return <footer>
-      <p>2024 | BotStore (Backend) - v0.5.8 | BotStore (Frontend) - v0.1.0</p>
+      <p>BotStore (Backend) - v0.5.8 BETA | BotStore (Frontend) - v0.1.0 BETA</p>
    </footer>
 }
