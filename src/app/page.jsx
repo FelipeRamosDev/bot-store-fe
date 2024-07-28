3b1b7d855ed4e910d70fd0bfa2d6c31abddcc3a8
FelipeRamosDev <felipe@feliperamos.info>
2024-07-28 04:41:03 -0300

    [BOTF-10] Features Brief
    ## [BOTF-10] Description
    Creating the section for the features brief description

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
 import BasePage from "@/templates/basePage/BasePage";
 import HomeTopBanner from "@/components/banners/homeTopBanner/HomeTopBanner";
+import HomeContent from "@/components/content/home/Home";
 
 export default function HomePage() {
    return <BasePage>
       <HomeTopBanner />
+
+      <HomeContent />
    </BasePage>;
 }
