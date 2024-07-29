92ab1efaa0df63c62ab6b9a1bb012e4f018a54cb
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-07-29 20:51:33 -0300

    [BOTF-18] Activities History Table (#15)
    ## [BOTF-18] Description
    Created the table for the activities

--- src/components/content/dashboard/home/DashboardSidebar.jsx
+++ src/components/content/dashboard/home/DashboardSidebar.jsx
@@ -1,11 +1,22 @@
+import ActivitiesTable from "@/components/tables/activitiesTable/ActivitiesTable";
 import UserInstance from "@/components/tiles/userInstance/Userinstance";
 
 const DUMMY_USER_INSTANCE = {
    status: 'online'
 };
 
+const DUMMY_ACTIVITIES = [
+   { type: 'message', subject: 'faiaofnasiofasin', summary: 'fdsfsd' },
+   { type: 'error', subject: 'faiaofnasiofasin', summary: 'fdsfsd' },
+   { type: 'message', subject: 'faiaofnasiofasin', summary: 'fdsfsd' },
+   { type: 'message', subject: 'faiaofnasiofasin', summary: 'fdsfsd' },
+   { type: 'warn', subject: 'faiaofnasiofasin', summary: 'fdsfsd' },
+   { type: 'message', subject: 'faiaofnasiofasin', summary: 'fdsfsd' },
+]
+
 export default function DashboardSidebar() {
    return <>
       <UserInstance instance={DUMMY_USER_INSTANCE} />
+      <ActivitiesTable activities={DUMMY_ACTIVITIES} />
    </>;
 }
---
+++ src/components/tables/activitiesTable/ActivitiesTable.jsx
@@ -0,0 +1,32 @@
+'use client';
+import "./ActivitiesTable.scss";
+import StatusBadge from "@/components/common/statusBedge/StatusBadge";
+import TableBase from '@/components/tables/tableBase/TableBase';
+
+export default function ActivitiesTable({ activities }) {
+   return <div className="activities-table" padding="s" radius="l">
+      <h3 className="section-title">Activities</h3>
+
+      <TableBase
+         items={activities}
+         hideHeader={true}
+         headerConfigs={[
+            {
+               propKey: 'type',
+               style: { width: '4rem' },
+               format: (__, item) => {
+                  return <StatusBadge>{item.type?.toUpperCase()}</StatusBadge>;
+               }
+            },
+            {
+               format: (__, item) => {
+                  return <>
+                     <p>{item.subject}</p>
+                     <p>{item.summary}</p>
+                  </>;
+               }
+            }
+         ]}
+      />
+   </div>
+}
