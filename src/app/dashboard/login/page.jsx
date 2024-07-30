df4049224d6785f432b662ad282c4acc40d6eb1e
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-07-30 17:26:40 -0300

    [BOTF-25] Create the page itself (#20)
    ## [BOTF-25] Description
    Created the login page

---
+++ src/app/dashboard/login/page.jsx
@@ -0,0 +1,8 @@
+import Login from "@/components/content/dashboard/login/Login";
+import BasePage from "@/templates/basePage/BasePage";
+
+export default function LoginPage() {
+   return <BasePage className="login-page">
+      <Login />
+   </BasePage>;
+}
