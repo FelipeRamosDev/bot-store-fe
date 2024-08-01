e22b7c0b2241175ab739a50d84e5d52b9f27cb0d
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-08-01 15:50:19 -0300

    [BOTF-32] Sign out (#29)
    ## [BOTF-32] Description
    Created the sign-out function

--- src/components/headers/topHeader/TopHeader.jsx
+++ src/components/headers/topHeader/TopHeader.jsx
@@ -1,15 +1,17 @@
 import Logo from '@/components/common/Logo';
-import TopNav from './TopNav';
+import TopNavPublic from './TopNavPublic';
+import TopNavAuth from './TopNavAuth';
 import Link from 'next/link';
 
-export default function TopHeader({ fullContainer = true }) {
+export default function TopHeader({ type = 'public', fullContainer = true }) {
    return <header>
       <div className={fullContainer ? 'full-container' : 'container'}>
          <Link href="/">
             <Logo />
          </Link>
 
-         <TopNav />
+         {type === 'public' && <TopNavPublic />}
+         {type === 'auth' && <TopNavAuth />}
       </div>
    </header>
 }
--- src/templates/authBasePage/AuthBasePage.jsx
+++ src/templates/authBasePage/AuthBasePage.jsx
@@ -2,7 +2,7 @@ import { AuthUserProvider } from '@/contexts/AuthUser';
 import BasePage from '../basePage/BasePage';
 
 export default function AuthBasePage({ children }) {
-   return <BasePage>
+   return <BasePage headerMenu="auth">
       <AuthUserProvider>
          {children}
       </AuthUserProvider>
