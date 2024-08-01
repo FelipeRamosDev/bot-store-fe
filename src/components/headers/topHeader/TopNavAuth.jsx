297427f05aa51ed332f8686f90aae9ab26d95458
FelipeRamosDev <felipe@feliperamos.info>
2024-08-01 15:49:50 -0300

    [BOTF-32] Sign out
    ## [BOTF-32] Description
    Created the sign-out function

---
+++ src/components/headers/topHeader/TopNavAuth.jsx
@@ -0,0 +1,31 @@
+'use client';
+import { useContext } from 'react';
+import { useRouter } from 'next/navigation';
+import APIContext from '@/contexts/4HandsAPI';
+import Link from 'next/link';
+import CTAButton from '@/components/common/ctaButton/CTAButton';
+
+export default function TopNav() {
+   const instance = useContext(APIContext);
+   const router = useRouter();
+
+   async function signOut() {
+      try {
+         await instance.auth.signOut();
+         router.push('/');
+      } catch (error) {
+         throw error;
+      }
+   }
+
+   return <nav>
+      <Link href="/dashboard/my-bots">My Bots</Link>
+      <Link href="/dashboard/master-accounts">Master Accounts</Link>
+      <Link href="/slots">Slots</Link>
+      <Link href="/positions">Positions</Link>
+
+      <CTAButton size="large" onClick={signOut}>
+         SIGNOUT
+      </CTAButton>
+   </nav>;
+}
--- src/templates/basePage/BasePage.jsx
+++ src/templates/basePage/BasePage.jsx
@@ -2,9 +2,9 @@ import MainFooter from '@/components/footers/mainFooter/MainFooter';
 import './BasePage.scss';
 import TopHeader from '@/components/headers/topHeader/TopHeader';
 
-export default function BasePage({ className = '', fullContainer, children }) {
+export default function BasePage({ className = '', fullContainer, headerMenu, children }) {
    return (<>
-      <TopHeader fullContainer={fullContainer} />
+      <TopHeader fullContainer={fullContainer} type={headerMenu} />
 
       <div className={`page ${className}`}>
          {children}
