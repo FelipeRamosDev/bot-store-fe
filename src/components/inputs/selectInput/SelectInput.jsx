02b0236e8a4d1ad4f7eca2414a998aefc0e5f210
FelipeRamosDev <felipe@feliperamos.info>
2024-08-07 22:45:37 -0300

    [BOTF-64] Handle the Select Value
    ## [BOTF-64] Description
    Fixed the bug on handling the value

--- src/components/inputs/selectInput/SelectInput.jsx
+++ src/components/inputs/selectInput/SelectInput.jsx
@@ -5,8 +5,8 @@ import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 
-export default function SelectInput({ className = '', schema = {}, ...props }) {
-   const { label, value = '', options = [], onChange = () => {}, useNoneOption = false } = schema;
+export default function SelectInput({ className = '', schema = {}, onChange = () => {}, ...props }) {
+   const { label, value = '', options = [], useNoneOption = false } = schema;
    const [ displayValue, setDisplayValue ] = useState(value);
    const inputID = useRef();
 
@@ -14,22 +14,17 @@ export default function SelectInput({ className = '', schema = {}, ...props }) {
       inputID.current = Math.random();
    }
 
-   const handleChange = (ev) => {
-      const value = ev.target.value;
-      setDisplayValue(value);
-      onChange(ev);
-   };
 
    return <FormControl color="tertiary" className={`select-input ${className}`} variant="filled" {...props}>
       {label && <InputLabel id={inputID.current}>{label}</InputLabel>}
 
       <Select
          labelId={inputID.current}
-         value={displayValue}
-         onChange={handleChange}
+         defaultValue={value}
+         onChange={onChange}
       >
          {useNoneOption && <MenuItem value="">None</MenuItem>}
-         {options.map(opt => <MenuItem value={opt.value}>{opt.label}</MenuItem>)}
+         {options.map(opt => <MenuItem key={Math.random()} value={opt.value}>{opt.label}</MenuItem>)}
       </Select>
    </FormControl>;
 }
