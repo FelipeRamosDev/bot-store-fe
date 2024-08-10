fbbfb4271f3a4441b4cc85b52980340484ae5e3a
FelipeRamosDev <felipe@feliperamos.info>
2024-08-10 11:08:27 -0300

    [BOTF-80] Demo Deposits
    ## [BOTF-80] Description
    Created the demo deposit modal to perform deposits with demo money

---
+++ src/components/menus/dropdown/masterMenu/MasterMenu.helper.js
@@ -0,0 +1,31 @@
+export async function handleDemoDeposit() {
+   try {
+      
+   } catch (err) {
+      throw err;
+   }
+}
+
+export async function handleEdit() {
+   try {
+      
+   } catch (err) {
+      throw err;
+   }
+}
+
+export async function handleArchive() {
+   try {
+      
+   } catch (err) {
+      throw err;
+   }
+}
+
+export async function handleDelete() {
+   try {
+      
+   } catch (err) {
+      throw err;
+   }
+}
