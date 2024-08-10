c7bd4008e8da1b7c28a7e8edc325f4e79118961e
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-08-10 10:08:04 -0300

    [BOTF-79] Master Dropdown Menu (#74)
    ## [BOTF-79] Description
    Created and implemented the menu without the buttons action

---
+++ src/components/menus/dropdown/masterMenu/MasterMenu.jsx
@@ -0,0 +1,83 @@
+import './MasterMenu.scss';
+import { useState } from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Tooltip from '@mui/material/Tooltip';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import RoundIconButton from '@/components/buttons/roundButton/RoundIconButton';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import StopIcon from '@mui/icons-material/Stop';
+import { Divider } from '@mui/material';
+
+export default function MasterMenu({ isDemo = false }) {
+   const [ anchorEl, setAnchorEl ] = useState(null);
+   const open = Boolean(anchorEl);
+
+   const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+   };
+
+   const handleClose = () => {
+      setAnchorEl(null);
+   };
+
+   return (
+      <>
+         <Tooltip title="Master's Menu">
+            <RoundIconButton Icon={MoreVertIcon} onClick={handleClick} />
+         </Tooltip>
+
+         <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+         >
+            {isDemo && <MenuItem onClick={handleClose}>
+               <ListItemIcon>
+                  <AttachMoneyIcon fontSize="small" />
+               </ListItemIcon>
+               Demo Deposit
+            </MenuItem>}
+            <MenuItem onClick={handleClose} disabled>
+               <ListItemIcon>
+                  <PlayArrowIcon fontSize="small" />
+               </ListItemIcon>
+               Start All
+            </MenuItem>
+            <MenuItem onClick={handleClose} disabled>
+               <ListItemIcon>
+                  <StopIcon fontSize="small" />
+               </ListItemIcon>
+               Stop All
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={handleClose}>
+               <ListItemIcon>
+                  <ModeEditIcon fontSize="small" />
+               </ListItemIcon>
+               Edit
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+               <ListItemIcon>
+                  <ArchiveIcon fontSize="small" />
+               </ListItemIcon>
+               Archive
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+               <ListItemIcon>
+                  <DeleteForeverIcon color="error" fontSize="small" />
+               </ListItemIcon>
+               Delete Master
+            </MenuItem>
+         </Menu>
+      </>
+   );
+}
