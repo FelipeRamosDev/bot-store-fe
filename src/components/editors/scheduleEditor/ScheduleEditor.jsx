7ee4bf185aa01c1693a00ca6266dff96e4f1f4e6
FelipeRamosDev <felipe@feliperamos.info>
2024-08-15 11:30:06 -0300

    [BOTF-98] Adjusts on schedule due to BE fix for it
    ## [BOTF-98] Description
    Made adjustments in order to follow the fix on [BOT-461]

--- src/components/editors/scheduleEditor/ScheduleEditor.jsx
+++ src/components/editors/scheduleEditor/ScheduleEditor.jsx
@@ -5,13 +5,17 @@ import TopBorderButton from '@/components/buttons/topBorderButton/TopBorderButto
 import CreateScheduleForm from '@/components/forms/createScheduleForm/CreateScheduleForm';
 import DBQueryContext from '@/contexts/DBQuery';
 import NoDocumentsTile from '@/components/tiles/noDocumentsTile/NoDocumentsTile';
+import PrettyDate from '@/components/displays/prettyDate/PrettyDate';
 
 export default function ScheduleEditor({ masterUID, editorState, setEditorState }) {
    const { query = [] } = useContext(DBQueryContext);
+   const goalsSchedule = query.find(item => item.type === 'goals' && item.isActive);
+   const runtimeSchedules = query.filter(item => item._id !== goalsSchedule?._id);
 
    if (editorState === 'display') {
       return <div className="schedules-editor">
-         {query.map((doc) => <ScheduleTile key={Math.random()} schedule={doc} />)}
+         {runtimeSchedules.map((doc) => <ScheduleTile key={Math.random()} schedule={doc} />)}
+         {goalsSchedule && <ScheduleTile schedule={goalsSchedule} setView={setEditorState} />}
 
          {!query.length && <NoDocumentsTile Icon={false} message="There is any schedules created yet!" noBorder={true} />}
       </div>
@@ -19,7 +23,7 @@ export default function ScheduleEditor({ masterUID, editorState, setEditorState 
 
    if (editorState === 'edit') {
       return <div className="schedules-editor">
-         {query.map((doc) => <div key={Math.random()} className="edit-wrap">
+         {runtimeSchedules.map((doc) => <div key={Math.random()} className="edit-wrap">
             <ScheduleTile editMode={true} schedule={doc} setView={setEditorState} />
          </div>)}
 
