22fdc2d9b3813de2c675cee053f08b825bbe471d
FelipeRamosDev <felipe@feliperamos.info>
2024-08-22 16:12:43 -0300

    [BOTF-135] content

--- src/components/content/dashboard/home/DashboardHome.jsx
+++ src/components/content/dashboard/home/DashboardHome.jsx
@@ -1,4 +1,5 @@
 'use client';
+
 import './DashboardHome.scss';
 import { useState } from 'react';
 import DashboardHeader from "./DashboardHeader";
@@ -6,6 +7,19 @@ import DashboardContent from "./DashboardContent";
 import DashboardSidebar from "./DashboardSidebar";
 import ContentSidebar from "@/components/layout/contentSidebar/ContentSidebar";
 
+/**
+ * DashboardHome component serves as the main layout for the dashboard page.
+ * 
+ * It includes:
+ * - `DashboardHeader`: A header component with functionality for creating a new master account.
+ * - `DashboardContent`: The main content area of the dashboard, displaying data related to master accounts, slots, and positions.
+ * - `DashboardSidebar`: A sidebar component for additional navigation or controls.
+ * - `ContentSidebar`: A layout component that wraps the dashboard content and sidebar with additional styling.
+ * 
+ * It maintains local state to control the visibility of the "Create Master" modal.
+ * 
+ * @returns {JSX.Element} The rendered dashboard layout with header, content, and sidebar.
+ */
 export default function DashboardHome() {
    const [ createMasterModal, setCreateMasterModal ] = useState(false);
 
