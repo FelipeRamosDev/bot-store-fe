6a73ab35a8a875a001e701564dc74d647583b16b
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-08-22 15:48:15 -0300

    [BOTF-130] /templates/** (#121)
    ## [BOTF-130] Documentation

--- src/templates/authBasePage/AuthBasePage.jsx
+++ src/templates/authBasePage/AuthBasePage.jsx
@@ -1,10 +1,22 @@
 import { AuthUserProvider } from '@/contexts/AuthUser';
 import BasePage from '../basePage/BasePage';
 
+/**
+ * `AuthBasePage` is a wrapper component that provides a base layout for pages that require authentication.
+ * It wraps its children with an `AuthUserProvider` to provide authentication context and a `BasePage` component
+ * to ensure consistent layout with an authentication-specific header menu.
+ *
+ * @param {Object} props - The props for the component.
+ * @param {React.ReactNode} props.children - The content to be rendered inside the `AuthUserProvider` and `BasePage`.
+ *
+ * @returns {JSX.Element} The rendered `BasePage` component containing the `AuthUserProvider` and children.
+ */
 export default function AuthBasePage({ children }) {
-   return <BasePage headerMenu="auth">
-      <AuthUserProvider>
-         {children}
-      </AuthUserProvider>
-   </BasePage>
+   return (
+      <BasePage headerMenu="auth">
+         <AuthUserProvider>
+            {children}
+         </AuthUserProvider>
+      </BasePage>
+   );
 }
--- src/templates/basePage/BasePage.jsx
+++ src/templates/basePage/BasePage.jsx
@@ -2,14 +2,28 @@ import MainFooter from '@/components/footers/mainFooter/MainFooter';
 import './BasePage.scss';
 import TopHeader from '@/components/headers/topHeader/TopHeader';
 
+/**
+ * `BasePage` is a layout component that includes a top header and a main footer. 
+ * It serves as a wrapper for the main content of a page, providing a consistent layout structure.
+ *
+ * @param {Object} props - The props for the component.
+ * @param {string} [props.className=''] - Optional additional CSS class names to apply to the main content container.
+ * @param {boolean} [props.fullContainer=false] - Determines if the container should span the full width of the viewport.
+ * @param {string} [props.headerMenu] - Optional type for the `TopHeader` component to determine the header menu configuration.
+ * @param {React.ReactNode} props.children - The content to be rendered within the `BasePage`.
+ *
+ * @returns {JSX.Element} The rendered `BasePage` component with a `TopHeader`, content area, and `MainFooter`.
+ */
 export default function BasePage({ className = '', fullContainer, headerMenu, children }) {
-   return (<>
-      <TopHeader fullContainer={fullContainer} type={headerMenu} />
+   return (
+      <>
+         <TopHeader fullContainer={fullContainer} type={headerMenu} />
 
-      <div className={`page ${className}`}>
-         {children}
-      </div>
+         <div className={`page ${className}`}>
+            {children}
+         </div>
 
-      <MainFooter className="absolute-bottom" />
-   </>);
+         <MainFooter className="absolute-bottom" />
+      </>
+   );
 }
