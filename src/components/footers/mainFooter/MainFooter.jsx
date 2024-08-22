b05f7aea9317583bfc5205c9362785b7c341ff4d
FelipeRamosDev <felipe@feliperamos.info>
2024-08-22 19:34:28 -0300

    [BOTF-122] Fixing duplicated code by mistake

--- src/components/footers/mainFooter/MainFooter.jsx
+++ src/components/footers/mainFooter/MainFooter.jsx
@@ -27,9 +27,3 @@ export default function MainFooter({ ...props }) {
       </footer>
    );
 }
-
-export default function MainFooter({ ...props }) {
-   return <footer {...props}>
-      <p>BotStore (API) - v0.5.9 BETA | BotStore (Frontend) - v0.1.0 BETA</p>
-   </footer>
-}
