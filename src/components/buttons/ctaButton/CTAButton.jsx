1fe6d6bf395fe70965d84d0b8b9e7094cfd7cefb
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-08-22 16:00:39 -0300

    [BOTF-133] buttons (#124)

--- src/components/buttons/ctaButton/CTAButton.jsx
+++ src/components/buttons/ctaButton/CTAButton.jsx
@@ -1,11 +1,25 @@
 import './CTAButton.scss';
 import { Button } from "@mui/material";
 
+/**
+ * `CTAButton` is a call-to-action button component with predefined styling and variant.
+ * It renders a MUI `Button` with the "contained" variant and "tertiary" color.
+ *
+ * @param {Object} props - The props object.
+ * @param {React.ReactNode} [props.children] - The content to be displayed inside the button.
+ * @param {Object} [props] - Additional props to be passed to the MUI `Button` component.
+ *
+ * @returns {JSX.Element} The rendered `CTAButton` component.
+ */
 export default function CTAButton({ children, ...props }) {
-   return <Button
-      className="cta-button"
-      variant="contained"
-      color="tertiary"
-      {...props}
-   >{children}</Button>
+   return (
+      <Button
+         className="cta-button"
+         variant="contained"
+         color="tertiary"
+         {...props}
+      >
+         {children}
+      </Button>
+   );
 }
--- src/components/buttons/spinnerButton/SpinnerButton.jsx
+++ src/components/buttons/spinnerButton/SpinnerButton.jsx
@@ -1,5 +1,17 @@
 import LoadingButton from '@mui/lab/LoadingButton';
 
+/**
+ * `SpinnerButton` is a styled button component that incorporates loading indicators using MUI's `LoadingButton`.
+ * It adjusts padding based on the loading state and position of the spinner.
+ *
+ * @param {Object} props - The props object.
+ * @param {boolean} [props.loading=false] - If true, shows a loading spinner within the button.
+ * @param {'start' | 'end'} [props.loadingPosition='end'] - The position of the loading spinner relative to the button content. Can be 'start' or 'end'.
+ * @param {React.ReactNode} [props.children] - The content to be displayed inside the button.
+ * @param {Object} [props] - Additional props to be passed to the MUI `LoadingButton` component.
+ *
+ * @returns {JSX.Element} The rendered `SpinnerButton` component.
+ */
 export default function SpinnerButton({ loading = false, loadingPosition = 'end', children, ...props}) {
    const style = { padding: '0.89rem 1.5rem' };
 
