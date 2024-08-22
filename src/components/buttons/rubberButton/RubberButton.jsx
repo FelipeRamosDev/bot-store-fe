91a2e92cc344684a31f2f768e908c2452b1ef8af
FelipeRamosDev <felipe@feliperamos.info>
2024-08-22 16:00:17 -0300

    [BOTF-133] buttons

--- src/components/buttons/rubberButton/RubberButton.jsx
+++ src/components/buttons/rubberButton/RubberButton.jsx
@@ -1,12 +1,25 @@
 import './RubberButton.scss';
 import Button from '@mui/material/Button';
 
+/**
+ * `RubberButton` is a styled button component that utilizes MUI's `Button` component with a "contained" variant.
+ * It applies custom styling from the `RubberButton.scss` file and allows additional props to be passed through.
+ *
+ * @param {Object} props - The props object.
+ * @param {string} [props.className] - Additional CSS class names to apply to the button.
+ * @param {React.ReactNode} [props.children] - The content to be displayed inside the button.
+ * @param {Object} [props] - Additional props to be passed to the MUI `Button` component.
+ *
+ * @returns {JSX.Element} The rendered `RubberButton` component.
+ */
 export default function RubberButton({ className = '', children, ...props }) {
-   return <Button
-      className={`rubber-button ${className}`}
-      variant="contained"
-      {...props}
-   >
-      {children}
-   </Button>
+   return (
+      <Button
+         className={`rubber-button ${className}`}
+         variant="contained"
+         {...props}
+      >
+         {children}
+      </Button>
+   );
 }
