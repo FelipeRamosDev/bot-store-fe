5989edd16c956842156e7e5d4f7ed773ea054e23
FelipeRamosDev <felipe@feliperamos.info>
2024-08-28 16:13:55 -0300

    [BOTF-157] Remove Limits from DB Queries on Dashboard Home
    ## [BOTF-157] Description
    Removed the limits

--- src/components/content/dashboard/home/DashboardContent.jsx
+++ src/components/content/dashboard/home/DashboardContent.jsx
@@ -33,7 +33,6 @@ export default function DashboardContent({ createMasterModal }) {
          collection="master_accounts"
          filter={{ user: user._id }}
          sort={{ pnl: -1 }}
-         limit={10}
          subscribe={true}
       >
          <MastersGrid createMasterModal={createMasterModal} />
@@ -49,7 +48,6 @@ export default function DashboardContent({ createMasterModal }) {
                collection="slots"
                filter={{ user: user._id }}
                sort={{ pnl: -1 }}
-               limit={7}
                subscribe={true}
             >
                <SlotsTable />
@@ -64,7 +62,6 @@ export default function DashboardContent({ createMasterModal }) {
                collection="positions"
                filter={{ user: user._id }}
                sort={{ modifiedAt: -1 }}
-               limit={7}
                subscribe={true}
             >
                <PositionsTable include={['symbol', 'type', 'usedLeverage', 'pnl']} />
