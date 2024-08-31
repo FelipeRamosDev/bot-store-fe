e27c62e5bbeb65da17884d83406e5a62befe52b3
FelipeRamosDev <felipe@feliperamos.info>
2024-08-31 14:50:07 -0300

    [BOTF=121] Adding Details Icon

--- src/components/modals/quickviews/SlotQuickview/SlotQuickviewDetails.jsx
+++ src/components/modals/quickviews/SlotQuickview/SlotQuickviewDetails.jsx
@@ -2,13 +2,14 @@ import Card from '@/components/common/card/Card';
 import StatusBadge from '@/components/common/statusBedge/StatusBadge';
 import ContentHeader from '@/components/headers/contentHeader/ContentHeader';
 import ContainedTable from '@/components/tables/containedTable/ContainedTable';
+import DisplaySettingsIcon from '@mui/icons-material/DisplaySettings';
 
 export default function SlotQuickviewDetails({ slot = {} }) {
    const symbol = slot.assets.length ? slot.assets[0] : '';
 
    return <Card padding="xs" radius="s" elevation={30}>
       <ContentHeader>
-         <h3 className="card-title">Details</h3>
+         <DisplaySettingsIcon /> <h3 className="card-title">Details</h3>
       </ContentHeader>
 
       <ContainedTable
