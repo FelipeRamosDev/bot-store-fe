bd86d419844171e0fc7dbc53579efbbab35d015d
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-09-06 16:28:00 -0300

    [BOTF-190] Bot Thread Forms (#178)
    ## [BOTF-190] Description
    Managed to create and delete a bot thread

--- src/components/tiles/bot/botThread/BotThread.jsx
+++ src/components/tiles/bot/botThread/BotThread.jsx
@@ -2,20 +2,62 @@
 import './BotThread.scss';
 import { useContext } from 'react';
 import DBQueryContext from '@/contexts/DBQuery';
+import APIContext from '@/contexts/4HandsAPI';
 import ContentHeader from '@/components/headers/contentHeader/ContentHeader';
 import WatermarkPriceCard from '@/components/common/watermarkPriceCard/WatermarkPriceCard';
 import HorizontalSplitIcon from '@mui/icons-material/HorizontalSplit';
 import Rule from '@/components/tiles/bot/rule/Rule';
 import Block from '../block/Block';
 import configs from '@/config.json';
+import NoDocumentsTile from '../../noDocumentsTile/NoDocumentsTile';
+import RoundIconButton from '@/components/buttons/roundButton/RoundIconButton';
+import Add from '@mui/icons-material/Add';
+import LogicalOperatorForm from '@/components/forms/logicalOperatorForm/LogicalOperatorForm';
+import Delete from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
 
 export default function BotThread({ threadID, title, color, ...props }) {
+   const API = useContext(APIContext);
    const { doc = {} } = useContext(DBQueryContext);
    const evalThread = doc.eval;
    const threadBlock = evalThread && evalThread[threadID];
    const thread = threadBlock && threadBlock.thread;
    const watermarkSize = window.innerWidth < configs.breakpoints.s ? 23 : 28;
    const paddingSize = window.innerWidth < configs.breakpoints.s ? 3 : 14;
+
+   async function createThread() {
+      try {
+         const created = await API.ajax.authPut('/bot/add-thread', {
+            eventName: threadID,
+            botUID: doc._id
+         });
+
+         if (created.error) {
+            throw created;
+         }
+      } catch (err) {
+         throw err;
+      }
+   }
+
+   async function handleDelete() {
+      try {
+         const deleted = await API.ajax.authDelete('/bot/delete-block', {
+            blockUID: thread._id,
+            botUID: doc._id
+         });
+
+         if (deleted.error) {
+            throw deleted;
+         }
+      } catch (err) {
+         throw err;
+      }
+   }
+
+   if (!thread && (threadID === 'openLong' || threadID === 'openShort')) {
+      return <NoDocumentsTile message={title} onClick={createThread} />;
+   }
 
    if (!thread) {
       return <></>;
@@ -33,7 +75,13 @@ export default function BotThread({ threadID, title, color, ...props }) {
          paddingSize={paddingSize}
          {...props}
       >
-         <ContentHeader>
+         <ContentHeader Toolbar={() => <>
+            <LogicalOperatorForm botUID={doc._id} block={thread} />
+
+            <Button className="delete-button" color="info" onClick={handleDelete}>
+               <Delete />
+            </Button>
+         </>}>
             <HorizontalSplitIcon fontSize="small" /> <h5 className="card-title">{title}</h5>
          </ContentHeader>
 
@@ -47,6 +95,10 @@ export default function BotThread({ threadID, title, color, ...props }) {
                   return <Rule key={child._id} rule={child} index={index} logicalOperator={thread.ifType} />;
                }
             })}
+            
+            <div className="add-new">
+               <RoundIconButton Icon={Add} color="tertiary" variant="contained" />
+            </div>
          </div>
       </WatermarkPriceCard>
    );
