e972bbe45c2482d79b383cb7062fc436a0219365
FelipeRamosDev <felipe@feliperamos.info>
2024-09-11 21:01:12 -0300

    [BOTF-192] Bot Status Transition
    ## [BOTF-192] Description
    Created the statuses transition for the bot between: draft, private and public

--- src/components/content/dashboard/bots/botDetails/BotDetailsHeader.jsx
+++ src/components/content/dashboard/bots/botDetails/BotDetailsHeader.jsx
@@ -2,9 +2,28 @@
 import { useContext } from 'react';
 import DBQueryContext from '@/contexts/DBQuery';
 import Card from '@/components/common/card/Card';
+import CheckButtonGroupInput from '@/components/inputs/checkButtonGroupInput/CheckButtonGroupInput';
+import ContainedTable from '@/components/tables/containedTable/ContainedTable';
+import APIContext from '@/contexts/4HandsAPI';
 
 export default function BotDetailsHeader() {
    const { doc = {} } = useContext(DBQueryContext);
+   const API = useContext(APIContext);
+
+   async function handleStatusChange({ target: { value }}) {
+      try {
+         const changed = await API.ajax.authPost('/bot/status-transition', {
+            botUID: doc._id,
+            newStatus: value
+         });
+
+         if (changed.error) {
+            throw changed;
+         }
+      } catch (err) {
+         throw err;
+      }
+   }
 
    return <div className="page-header">
       <div className="cover"></div>
@@ -19,7 +38,26 @@ export default function BotDetailsHeader() {
             </div>
 
             <Card className="infos" padding="s" elevation={40}>
-               
+               {doc.status && <CheckButtonGroupInput
+                  onChange={handleStatusChange}
+                  schema={{
+                     key: 'status',
+                     defaultValue: doc.status,
+                     options: [
+                        { label: 'Draft', value: 'draft' },
+                        { label: 'Private', value: 'private' },
+                        { label: 'Public', value: 'public' }
+                     ]
+                  }}
+               />}
+
+               <ContainedTable
+                  tableData={[
+                     { label: 'ID', value: doc.cod },
+                     { label: 'Created At', value: new Date(doc.createdAt).toLocaleString() },
+                     { label: 'Modified At', value: new Date(doc.modifiedAt).toLocaleString() },
+                  ]}
+               />
             </Card>
          </div>
       </div>
