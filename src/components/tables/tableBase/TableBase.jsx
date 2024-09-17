5872b1f4cb3a942287a7a75ba02a0a9096ffa557
FelipeRamosDev <felipe@feliperamos.info>
2024-09-17 17:05:54 -0300

    [BOTF-227] See More Pagination
    ## [BOTF-227] Description
    Created the see more feature

--- src/components/tables/tableBase/TableBase.jsx
+++ src/components/tables/tableBase/TableBase.jsx
@@ -10,6 +10,7 @@ import TableBaseHeader from './TableBaseHeader';
 import ColumnConfig from '@/models/TableColumnConfig';
 import FitSpinner from '@/components/load/fitSpinner/FitSpinner';
 import NoDocumentsTile from '@/components/tiles/noDocumentsTile/NoDocumentsTile';
+import { Button } from '@mui/material';
 
 /**
  * A reusable table component that handles pagination, loading states, and customizable rows and headers.
@@ -24,7 +25,8 @@ import NoDocumentsTile from '@/components/tiles/noDocumentsTile/NoDocumentsTile'
  * @param {boolean} [props.loading=true] - Whether the table is in a loading state.
  * @param {Function} [props.onClickRow=()=>{}] - Function to handle the row's click event. It receives the current item as the first argument.
  * @param {number} [props.itemsPerPage] - The number of items to list per page.
- * @param {boolean} [props.usePagination] - Set to true if you need the pagination footer.
+ * @param {boolean} [props.usePagination=false] - Set to true if you need the pagination footer.
+ * @param {boolean} [props.useSeeMorePage=false] - Set to true if you need the "see more" button to paginate.
  * @param {Array} [props.headerConfigs] - Configuration for table headers.
  * @param {React.Component} [props.CustomTableItem] - Custom component for rendering table rows.
  * @param {Array} [props.include] - Optional array of column keys to include.
@@ -43,7 +45,8 @@ export default function TableBase({
    onPageNav = async () => {},
    onRowsPerPageChange = async () => {},
    itemsPerPage = 10,
-   usePagination,
+   usePagination = false,
+   useSeeMorePage = false,
    headerConfigs,
    CustomTableItem,
    include,
@@ -52,12 +55,19 @@ export default function TableBase({
 }) {
    const [ page, setPage ] = useState(0);
    const [ rowsPerPage, setRowsPerPage ] = useState(itemsPerPage);
-   const slicedSlots = items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+   const slicedSlots = useSeeMorePage ? items : items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
    const TableItem = CustomTableItem || TableBaseRow;
    const rowsPerPageCount = useRef();
+   const tableContainer = useRef();
+   let tableHeight;
+   let disableSeeMore = false;
 
    if (!rowsPerPageCount.current) {
       rowsPerPageCount.current = rowsPerPage;
+   }
+
+   if (items.length < ((page + 1) * itemsPerPage)) {
+      disableSeeMore = true
    }
 
    const rowsPerPageOptions = [1,2,3,4,5,6].map((_, index) => rowsPerPageCount.current * (index + 1));
@@ -73,6 +83,10 @@ export default function TableBase({
       setRowsPerPage(newValue);
    }
 
+   if (tableContainer.current) {
+      tableHeight = tableContainer.current.clientHeight;
+   }
+
    if (Array.isArray(include)) {
       headerConfigs = headerConfigs.filter(item => include.some(inc => inc === item.propKey))
    }
@@ -84,9 +98,9 @@ export default function TableBase({
    headerConfigs = headerConfigs.map(item => new ColumnConfig(item));
 
    return <div className={`table-base ${className} ${elevate ? 'elevate' : ''} ${borderLastRow ? 'lastrow-noborder' : ''}`} {...props}>
-      {loading && <FitSpinner color="tertiary-dark" spinner="Loading" />}
+      {loading && <FitSpinner color="tertiary-dark" spinner="Loading" style={{ minHeight: tableHeight }} />}
 
-      {!loading && <TableContainer sx={{ maxHeight }}>
+      {!loading && <TableContainer ref={tableContainer} sx={{ maxHeight }}>
          <Table stickyHeader>
             {!hideHeader && <TableBaseHeader headerConfigs={headerConfigs} />}
 
@@ -110,14 +124,28 @@ export default function TableBase({
          <NoDocumentsTile noBorder={true} Icon={false} message={`There is no documents to list!`} />
       )}
 
-      {usePagination && <TablePagination
-         rowsPerPageOptions={rowsPerPageOptions}
-         component="div"
-         count={items.length}
-         rowsPerPage={rowsPerPage}
-         page={page}
-         onPageChange={handleChangePage}
-         onRowsPerPageChange={handleChangeRowsPerPage}
-      />}
+      {usePagination && (
+         <TablePagination
+            rowsPerPageOptions={rowsPerPageOptions}
+            component="div"
+            count={items.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+         />
+      )}
+
+      {useSeeMorePage && !disableSeeMore && (
+         <Button
+            className="seemore-button"
+            color="rubber"
+            variant="contained"
+            fullWidth={true}
+            onClick={(ev) => handleChangePage(ev, page + 1)}
+         >
+            See More
+         </Button>
+      )}
    </div>;
 }
