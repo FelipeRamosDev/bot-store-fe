3d8d2b0e6899ec3e31e231d75ca9060799e90eac
FelipeRamosDev <felipe@feliperamos.info>
2024-09-21 20:42:14 -0300

    [BOTF-212] Creating file of the tiles image

--- src/components/content/home/Home.jsx
+++ src/components/content/home/Home.jsx
@@ -2,8 +2,7 @@ import './Home.scss';
 import CTAButton from '@/components/buttons/ctaButton/CTAButton';
 import ContentSplit from '@/components/layout/contentSplit/ContentSplit';
 import HomeTopBanner from '@/components/banners/homeTopBanner/HomeTopBanner';
-import SlotTile from '@/components/tiles/slotTile/SlotTile';
-import btcDummy from '@/components/content/home/btcDummy.json';
+import SlotTilesImg from './img/SlotTilesImg';
 
 export default function HomeContent() {
    return (<>
@@ -16,45 +15,7 @@ export default function HomeContent() {
 
                <ContentSplit className="content">
                   <div className="column image-spot">
-                     <SlotTile
-                        dummyCandles={btcDummy}
-                        className="slot-a"
-                        demoMode={true}
-                        chartsDisplay={true}
-                        elevation={80}
-                        padding="s"
-                        slot={{
-                           cod: 'BA43245',
-                           name: 'Ethereum',
-                           bot: { name: 'Mr.Kaioh I', index: 1 },
-                           assets: ['ETHUSDT'],
-                           interval: '3m',
-                           status: 'running',
-                           pnl: 1123.45,
-                           totalRealizedPnl: 946,
-                           trades: [{ stopPrice: 62500, gainPrice: 64500 }]
-                        }}
-                     />
-
-                     <SlotTile
-                        dummyCandles={btcDummy}
-                        className="slot-b"
-                        demoMode={true}
-                        chartsDisplay={true}
-                        elevation={80}
-                        padding="s"
-                        slot={{
-                           cod: 'BA43245',
-                           name: 'Bitcoin',
-                           bot: { name: 'Mr.Kaioh II', index: 2 },
-                           assets: ['BTCUSDT'],
-                           interval: '15m',
-                           status: 'running',
-                           pnl: 8566,
-                           totalRealizedPnl: 7450,
-                           trades: [{ stopPrice: 62500, gainPrice: 64500 }]
-                        }}
-                     />
+                     <SlotTilesImg />
                   </div>
 
                   <div className="column marketing-cta right">
