4bf78a290fdc145ed3d243d2b56f67b6ef71aed0
FelipeRamosDev <felipe@feliperamos.info>
2024-09-25 11:25:43 -0300

    [BOTF-240] Slot Form Mobile View Issues
    ## [BOTF-240] Description
    Fixed the issues

--- src/models/Form/fieldTypes/TextFieldSchema.js
+++ src/models/Form/fieldTypes/TextFieldSchema.js
@@ -15,6 +15,7 @@ export default class TextFieldSchema extends FieldSchema {
     * Creates an instance of TextFieldSchema.
     *
     * @param {Object} setup - Configuration settings for the text field schema.
+    * @param {Object} setup.inputMode - The keyboard mode for mobiles.
     * @param {Function} form - The form instance that this field schema belongs to.
     */
    constructor(setup, form) {
@@ -22,6 +23,7 @@ export default class TextFieldSchema extends FieldSchema {
 
       this.type = String; // Specifies that the type of field is String.
       this.inputType = 'text'; // Specifies that the input type is 'text' for standard text input.
+      this.inputMode = inputMode || 'text';
 
       this.Input = TextInput; // Specifies the input component as TextInput.
    }
