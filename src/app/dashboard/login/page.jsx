77329b272dde444325e42d7a31c91bac27d354c2
FelipeRamosDev <felipe@feliperamos.info>
2024-09-27 15:48:11 -0300

    [BOTF-249] Remove mobile JS adjustments and use SCSS
    ## [BOTF-249] Description
    Added a CSS approach

--- src/app/dashboard/login/page.jsx
+++ src/app/dashboard/login/page.jsx
@@ -1,3 +1,4 @@
+import { APIProvider } from "@/contexts/4HandsAPI";
 import Login from "@/components/content/dashboard/login/Login";
 import BasePage from "@/templates/basePage/BasePage";
 
@@ -15,8 +16,10 @@ import BasePage from "@/templates/basePage/BasePage";
  */
 export default function LoginPage({ searchParams: { register } }) {
    return (
-      <BasePage className="login-page" fullContainer={false}>
-         <Login isRegister={Boolean(register)} />
-      </BasePage>
+      <APIProvider>
+         <BasePage className="login-page" fullContainer={false}>
+            <Login isRegister={Boolean(register)} />
+         </BasePage>
+      </APIProvider>
    );
 }
--- src/components/common/watermarkPriceCard/WatermarkPriceCard.jsx
+++ src/components/common/watermarkPriceCard/WatermarkPriceCard.jsx
@@ -29,76 +29,24 @@ import PriceCard from '@/components/common/priceCard/PriceCard';
  */
 export default function WatermarkPriceCard({
    className = '',
+   borderSide = 'left',
    watermark,
    absoluteHeader,
-   watermarkSize = 28,
-   borderSide = 'left',
-   paddingSize = 14,
+   watermarkSize,
+   paddingSize,
    children,
    ...props
 }) {
-   const [ waterMarkStyle, setWaterMarkStyle ] = useState({ fontSize: watermarkSize });
-   const [ cardStyle, setCardStyle ] = useState({ paddingTop: absoluteHeader });
-   const waterMark = useRef();
-
-   useEffect(() => {
-      if (waterMark.current) {
-         const offsetWidth = waterMark.current.offsetWidth;
-         const offsetHeight = waterMark.current.offsetHeight;
-         const paddingValue = offsetHeight + paddingSize;
-
-         switch (borderSide) {
-            case 'left':
-               const left = (offsetWidth / 2) - (offsetHeight / 3);
-
-               setCardStyle(prev => ({ ...prev, paddingLeft: paddingValue }));
-               setWaterMarkStyle(prev => ({
-                  ...prev,
-                  left: -left,
-                  bottom: (offsetWidth / 2) - (offsetHeight / 1.9)
-               }));
-               break;
-            case 'top':
-               const top = offsetHeight * 0.1;
-
-               setCardStyle(prev => ({ ...prev, paddingTop: paddingValue }));
-               setWaterMarkStyle(prev => ({
-                  ...prev,
-                  top: -top,
-                  left: -(offsetHeight * 0.05)
-               }));
-               break;
-            case 'bottom':
-               const bottom = offsetHeight * 0.1;
-
-               setCardStyle(prev => ({ ...prev, paddingBottom: paddingValue }));
-               setWaterMarkStyle(prev => ({
-                  ...prev,
-                  bottom: -bottom,
-                  left: -(offsetHeight * 0.05)
-               }));
-               break;
-            case 'right':
-               const right = (offsetWidth / 2) - (offsetHeight / 2.5);
-
-               setCardStyle(prev => ({ ...prev, paddingLeft: paddingValue }));
-               setWaterMarkStyle(prev => ({
-                  ...prev,
-                  right: -right,
-                  bottom: (offsetWidth / 2) - (offsetHeight / 1.9)
-               }));
-               break;
-         }
-      }
-   }, [waterMark, borderSide, paddingSize]);
+   const waterMarkStyle = { fontSize: watermarkSize };
+   const cardStyle = { paddingTop: absoluteHeader, fontSize: watermarkSize };
 
    return <PriceCard
-      className={`watermark-price-card ${className}`}
+      className={`watermark-price-card ${borderSide || ''} ${className}`}
       borderSide={borderSide}
       style={cardStyle}
       {...props}
    >
-      <span ref={waterMark} className="watermark" style={waterMarkStyle} border-side={borderSide}>{watermark}</span>
+      <span className="watermark" style={waterMarkStyle} border-side={borderSide}>{watermark}</span>
       {children}
    </PriceCard>;
 }
