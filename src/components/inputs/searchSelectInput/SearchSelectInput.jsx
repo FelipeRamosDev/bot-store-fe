ffc3589d2cbd10f8366a4f7a6d51c3872a6d80d7
FelipeRamosDev <felipe@feliperamos.info>
2024-11-10 20:50:53 -0300

    [BOTF-333] Implementing the new "allowedSymbols"
    ## [BOTF-333] Description
    Implemented the new field and the multi options

--- src/components/inputs/searchSelectInput/SearchSelectInput.jsx
+++ src/components/inputs/searchSelectInput/SearchSelectInput.jsx
@@ -2,17 +2,37 @@
 
 import { useState } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
-import { FormControl, FormHelperText, TextField } from '@mui/material';
+import { Chip, FormControl, FormHelperText, TextField } from '@mui/material';
 import { parseClassName } from '@/helpers/parser';
 
 export default function SearchSelectInput({ className = '', errors = [], schema = {}, fullWidth = true, onChange = () => {}, ...props }) {
-   const { label, options = [], style, inputType = 'text', placeholder, color = 'tertiary', ListItem = () => {} } = schema;
+   const { label, options = [], style, inputType = 'text', placeholder, color = 'tertiary', multiOptions = false, ListItem = () => {} } = schema;
    const [ open, setOpen ] = useState(false);
+   const [ value, setValue ] = useState();
    let inputMode = schema.inputMode;
+   const fixedOptions = [];
    let defaultValue;
 
    if (schema.form.editMode) {
-      defaultValue = schema.getEditValue();
+      const editValue = schema.getEditValue();
+
+      if (multiOptions) {
+         if (typeof editValue === 'string') {
+            defaultValue = [ editValue ];
+         }
+         
+         else if (Array.isArray(editValue)) {
+            defaultValue = editValue.map(item => options.find(opt => opt.value === item)).filter(item => item);
+         }
+      } else {
+         if (typeof editValue === 'string') {
+            defaultValue = options.find(opt => opt.value === editValue);
+         }
+         
+         else if (Array.isArray(editValue) && editValue.length) {
+            defaultValue = options.find(opt => opt.value === editValue[0]);
+         }
+      }
    }
 
    if (!schema.inputMode) {
@@ -33,16 +53,33 @@ export default function SearchSelectInput({ className = '', errors = [], schema 
 
 
    return (
-      <FormControl color="tertiary" className={parseClassName(className, [ 'search-select-input' ])} style={style} variant="filled" error={errors.length}>
+      <FormControl color="tertiary" className={parseClassName(className, ['search-select-input', multiOptions ? 'multi-options' : ''])} style={style} variant="filled" error={errors.length}>
          <Autocomplete
+            multiple={multiOptions}
             open={open}
             onOpen={handleOpen}
             onClose={handleClose}
             isOptionEqualToValue={(option, value) => option?.value === value?.value}
             getOptionLabel={(option) => option?.label}
             options={options.sort((a, b) => b.quoteVolume - a.quoteVolume)}
-            onChange={(ev, opt) => onChange({ target: { value: opt?.value }})}
-            ListboxProps={{className: parseClassName(props.className, [ 'options-wrap' ]) }}
+            value={value || defaultValue}
+            onChange={(ev, opt) => {
+               if (multiOptions) {
+                  const newValue = [
+                     ...fixedOptions,
+                     ...opt.filter((option) => !fixedOptions.includes(option)),
+                  ];
+   
+                  onChange({ target: { value: newValue.map(item => item.value) } });
+                  setValue(newValue);
+               } else {
+                  const newValue = opt?.value;
+
+                  onChange({ target: { value: newValue }});
+                  setValue(opt);
+               }
+            }}
+            ListboxProps={{ className: parseClassName(props.className, ['options-wrap']) }}
             renderOption={(props, option) => <ListItem option={option} itemProps={props} />}
             renderInput={(params) => (
                <TextField
@@ -59,6 +96,20 @@ export default function SearchSelectInput({ className = '', errors = [], schema 
                   {...props}
                />
             )}
+            renderTags={multiOptions ? (tagValue, getTagProps) => (
+               tagValue.map((option, index) => {
+                  const { key, ...tagProps } = getTagProps({ index });
+
+                  return (
+                     <Chip
+                        key={key}
+                        label={option.label}
+                        {...tagProps}
+                        disabled={fixedOptions.includes(option)}
+                     />
+                  );
+               })
+            ) : undefined}
          />
 
          {errors.map(err => <FormHelperText key={Math.random()}>{err?.message}</FormHelperText>)}
