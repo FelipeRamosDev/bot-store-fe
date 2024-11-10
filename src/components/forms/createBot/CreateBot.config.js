966a08d40021c1f4be0cddd91612364c09819c09
FelipeRamosDev <felipe@feliperamos.info>
2024-11-10 00:28:44 -0300

    [BOTF-332] Implementing the new "allowedIntervals"
    ## [BOTF-332] Description
    Implemented the new "allowedIntervals" field

--- src/components/forms/createBot/CreateBot.config.js
+++ src/components/forms/createBot/CreateBot.config.js
@@ -1,4 +1,5 @@
 import Form from '@/models/Form';
+import CheckButtonGroupSchema from '@/models/Form/fieldTypes/CheckButtonGroupSchema';
 import TextFieldSchema from '@/models/Form/fieldTypes/TextFieldSchema';
 
 const createBotForm = new Form({
@@ -11,6 +12,25 @@ const createBotForm = new Form({
       new TextFieldSchema({
          key: 'description',
          label: 'Summary'
+      }),
+      new CheckButtonGroupSchema({
+         key: 'allowedIntervals',
+         label: 'Allowed Intervals',
+         multiValue: true,
+         options: [
+            { label: '1min', value: '1m' },
+            { label: '3min', value: '3m' },
+            { label: '5min', value: '5m' },
+            { label: '15min', value: '15m' },
+            { label: '30min', value: '30m' },
+            { label: '1h', value: '1h' },
+            { label: '2h', value: '2h' },
+            { label: '4h', value: '4h' },
+            { label: '8h', value: '8h' },
+            { label: '12h', value: '12h' },
+            { label: '1d', value: '1d' },
+            { label: '1w', value: '1w' }
+         ]
       })
    ]
 });
